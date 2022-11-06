049eec1a4425683efbd6e1f110cc247c5e5ce31b
dev.lauer <dev.lauer@elnarion.de>
2022-11-06 16:31:41 +0100

    add integration test for use short class names parameter

---
+++ plantuml-generator-maven-plugin/src/it/sequencediagram/0009-use-short-class-names-it/verify.groovy
@@ -0,0 +1,11 @@
+File buildLog = new File( basedir, 'build.log' )
+assert buildLog.exists()
+assert buildLog.text.contains( "[INFO] Starting plantuml sequence diagram generation" )
+
+File buildDiagram = new File( basedir.absolutePath+'/target/generated-docs/testsequencediagram1.txt' )
+File testDiagram = new File( basedir.absolutePath+'/../../../../../plantuml-generator-util/src/test/resources/sequence/0001_basic_caller_with_long_class_names.txt' )
+assert buildDiagram.exists()
+assert testDiagram.exists()
+String buildDiagramText = buildDiagram.text 
+String testDiagramText =  testDiagram.text
+assert testDiagramText.replaceAll("\\s+", "").equals(buildDiagramText.replaceAll("\\s+", "")) 

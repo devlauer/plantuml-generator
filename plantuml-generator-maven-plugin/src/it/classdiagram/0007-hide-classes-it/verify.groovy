13976d6cd3bd9b4bd190f097d1e6e806fd447e0e
dev.lauer <dev.lauer@elnarion.de>
2022-11-06 14:46:54 +0100

    add integration test for hide classes parameter

---
+++ plantuml-generator-maven-plugin/src/it/classdiagram/0007-hide-classes-it/verify.groovy
@@ -0,0 +1,11 @@
+File buildLog = new File( basedir, 'build.log' )
+assert buildLog.exists()
+assert buildLog.text.contains( "[INFO] Starting plantuml generation" )
+
+File buildDiagram = new File( basedir.absolutePath+'/target/generated-docs/testdiagram1.txt' )
+File testDiagram = new File( basedir.absolutePath+'/../../../../../plantuml-generator-util/src/test/resources/class/0001_general_diagram.txt' )
+assert buildDiagram.exists()
+assert testDiagram.exists()
+String buildDiagramText = buildDiagram.text 
+String testDiagramText =  testDiagram.text
+assert testDiagramText.replaceAll("\\s+", "").equals(buildDiagramText.replaceAll("\\s+", "")) 

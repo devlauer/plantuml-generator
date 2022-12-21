b0ac894e99259f2910afbaf57df8c7af4d322911
dev.lauer <dev.lauer@elnarion.de>
2022-12-21 16:01:40 +0100

    refactor new diagram text builder

--- plantuml-generator-util/src/main/java/de/elnarion/util/plantuml/generator/classdiagram/internal/PlantUMLClassDiagramTextBuilder.java
+++ plantuml-generator-util/src/main/java/de/elnarion/util/plantuml/generator/classdiagram/internal/PlantUMLClassDiagramTextBuilder.java
@@ -25,51 +25,87 @@ public class PlantUMLClassDiagramTextBuilder {
 		// build the Plant UML String by calling the corresponding method on all
 		// PlantUMLDiagramElements
 		final StringBuilder builder = new StringBuilder();
-		builder.append("@startuml");
-		builder.append(System.lineSeparator());
-		builder.append(System.lineSeparator());
+		startUMLDiagram(builder);
+		addTwoSpaceLinesToBuilder(builder);
 
-		if (!plantUMLConfig.getAdditionalPlantUmlConfigs().isEmpty()) {
-			plantUMLConfig.getAdditionalPlantUmlConfigs().forEach(additionalPlantUmlConfig -> {
-				builder.append(additionalPlantUmlConfig);
-				builder.append(System.lineSeparator());
-			});
-			builder.append(System.lineSeparator());
-			builder.append(System.lineSeparator());
-		}
+		appendAdditionalPlantUMLConfigsToBuilder(builder);
 
-		final List<UMLClass> listToCompare = new ArrayList<>();
-		listToCompare.addAll(classes.values());
-		// because the ordered list could be changed in between, sort the list
-		Collections.sort(listToCompare, (o1, o2) -> o1.getName().compareTo(o2.getName()));
-		final Collection<UMLClass> classesList = listToCompare;
-		final List<UMLRelationship> relationships = new ArrayList<>();
+		final Collection<UMLClass> sortedClassesList = sortClasses();
 		// add all class information to the diagram (includes field and method
 		// information)
 		// because of the full qualified class names packages are added automatically
 		// and need not be added manually
-		for (final UMLClass clazz : classesList) {
+		List<UMLRelationship> relationships = getSortedRelationshipsFromClasses(sortedClassesList);
+		appendSortedClassesToBuilder(builder, sortedClassesList);
+		addTwoSpaceLinesToBuilder(builder);
+		addRelationshipsToBuilder(builder, relationships);
+		addHideToggles(builder, sortedClassesList, relationships);
+		addTwoSpaceLinesToBuilder(builder);
+		endUmlDiagram(builder);
+		return builder.toString();
+		
+	}
+
+	private void endUmlDiagram(final StringBuilder builder) {
+		// add diagram end to text
+		builder.append("@enduml");
+	}
+
+	private void startUMLDiagram(final StringBuilder builder) {
+		builder.append("@startuml");
+	}
+
+	private List<UMLRelationship> getSortedRelationshipsFromClasses(final Collection<UMLClass> sortedClassesList) {
+		final List<UMLRelationship> relationships = new ArrayList<>();
+		for (final UMLClass clazz : sortedClassesList) {
 			relationships.addAll(classesAndRelationships.get(clazz));
-			builder.append(clazz.getDiagramText());
-			builder.append(System.lineSeparator());
-			builder.append(System.lineSeparator());
 		}
-		builder.append(System.lineSeparator());
-		builder.append(System.lineSeparator());
+		sortRelationships(relationships);
+		return relationships;
+	}
+
+	private void appendSortedClassesToBuilder(final StringBuilder builder, final Collection<UMLClass> sortedClassesList) {
+		for (final UMLClass clazz : sortedClassesList) {
+			builder.append(clazz.getDiagramText());
+			addTwoSpaceLinesToBuilder(builder);
+		}
+	}
+
+	private void sortRelationships(final List<UMLRelationship> relationships) {
 		// because the ordered list could be changed in between, sort the list
 		Collections.sort(relationships, (o1, o2) -> o1.getDiagramText().compareTo(o2.getDiagramText()));
+	}
+
+	private void addRelationshipsToBuilder(final StringBuilder builder, final List<UMLRelationship> relationships) {
 		// add all class relationships to the diagram
 		for (final UMLRelationship relationship : relationships) {
 			builder.append(relationship.getDiagramText());
 			builder.append(System.lineSeparator());
 		}
-		addHideToggles(builder, classesList, relationships);
+	}
+
+	private void addTwoSpaceLinesToBuilder(final StringBuilder builder) {
 		builder.append(System.lineSeparator());
 		builder.append(System.lineSeparator());
-		// add diagram end to text
-		builder.append("@enduml");
-		return builder.toString();
-		
+	}
+
+	private Collection<UMLClass> sortClasses() {
+		final List<UMLClass> listToCompare = new ArrayList<>();
+		listToCompare.addAll(classes.values());
+		// because the ordered list could be changed in between, sort the list
+		Collections.sort(listToCompare, (o1, o2) -> o1.getName().compareTo(o2.getName()));
+		final Collection<UMLClass> classesList = listToCompare;
+		return classesList;
+	}
+
+	private void appendAdditionalPlantUMLConfigsToBuilder(final StringBuilder builder) {
+		if (!plantUMLConfig.getAdditionalPlantUmlConfigs().isEmpty()) {
+			plantUMLConfig.getAdditionalPlantUmlConfigs().forEach(additionalPlantUmlConfig -> {
+				builder.append(additionalPlantUmlConfig);
+				builder.append(System.lineSeparator());
+			});
+			addTwoSpaceLinesToBuilder(builder);
+		}
 	}
 
 	/**

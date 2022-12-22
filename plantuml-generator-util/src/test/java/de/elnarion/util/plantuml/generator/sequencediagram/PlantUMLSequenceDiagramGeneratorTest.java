24ab317fdd19ae328f35991e9f5494e07464540c
dev.lauer <dev.lauer@elnarion.de>
2022-12-22 14:47:45 +0100

    fix broken build

--- plantuml-generator-util/src/test/java/de/elnarion/util/plantuml/generator/sequencediagram/PlantUMLSequenceDiagramGeneratorTest.java
+++ plantuml-generator-util/src/test/java/de/elnarion/util/plantuml/generator/sequencediagram/PlantUMLSequenceDiagramGeneratorTest.java
@@ -191,7 +191,6 @@ class PlantUMLSequenceDiagramGeneratorTest {
 
 		// ACT
 		String generatedDiagram = generator.generateDiagramText();
-		System.out.println(generatedDiagram);
 
 		// ASSERT
 		assertAll(() -> assertNotNull(generatedDiagram), () -> assertEquals(expectedDiagramText.replaceAll("\\s+", ""),
